b238d762a2cd207414b558305ae9abc45f46a9e6
NameInikita <name.inikita@gmail.com>
2017-07-25 22:57:07 +0300

    Создание и удаление папок

---
+++ IO/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\");
+            dir.CreateSubdirectory("TestDir");
+
+            for (int i=0;i<9;i++)
+            {
+                dir.CreateSubdirectory(string.Format(@"TestDir\Folder-{0}", i));
+                Console.WriteLine("Created Folder-{0}",i);
+            }
+
+            Console.ReadKey();
+
+            //Удаление на классе-объекте Directory
+            //try
+            //{
+            //        Directory.Delete(@"C:\TestDir",true);//true гарантирует удаление подкаталогов
+            //    }
+            //catch(Exception e)
+            //    {
+            //        Console.WriteLine(e.Message);
+            //    }
+
+            for (int i=0;i<9;i++)
+            {
+                try
+                {
+                    Directory.Delete(string.Format(@"C:\TestDir\Folder-{0}", i));
+                    Console.WriteLine("Folder-{0} deleted",i);
+                }
+                catch(Exception e) {Console.WriteLine(e.Message);}
+            }
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
